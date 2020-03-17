b89d19a6b413003a65d2cfa4b8da063d0b0eeda1
Nach Mehta <nmehta@mediafly.com>
2020-03-16 17:21:53 -0700

    clean up and fix tests

--- src/List.js
+++ src/List.js
@@ -20,12 +20,7 @@ export default function List(props) {
                 <h2>{props.header}</h2>
             </header>
             <div className="List-cards">
-                {props.cards.map(card => (
-                    <Card
-                        key={card.id}
-                        title={card.title}
-                        content={card.content} />
-                ))}
+                {cards}
                 <button type="button" className="List-add-button">
                     + Add Random Card
                 </button>
